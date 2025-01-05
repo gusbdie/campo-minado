21a572176ea163a6a4e515ed56514e6d5e1d2fe7
gusbdie <gustavobdietrich@gmail.com>
2025-01-05 20:31:16 -0300

    Ajustes no inicio do projeto

--- src/modelo/Campo.java
+++ src/modelo/Campo.java
@@ -8,9 +8,9 @@ public class Campo {
     private final int linha;
     private final int coluna;
 
-    private boolean aberto;
-    private boolean minado;
-    private boolean marcado;
+    //private boolean aberto;
+    //private boolean minado;
+    //private boolean marcado;
 
     private List<Campo> vizinhos = new ArrayList<>();
 
