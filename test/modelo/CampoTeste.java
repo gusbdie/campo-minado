96bd303fbd27de6dfa7a0e97e4a896683158a5b1
gusbdie <gustavobdietrich@gmail.com>
2025-01-11 21:29:38 -0300

    Testando o método de abertura de jogo e abertura de campos livres de bombas

--- test/modelo/CampoTeste.java
+++ test/modelo/CampoTeste.java
@@ -1,10 +1,13 @@
 package modelo;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import excecao.ExplosaoException;
 
 
 public class CampoTeste {
@@ -46,5 +49,59 @@ public class CampoTeste {
     void testeAlternarMarcado(){
         campo.alternarMarcado();
         assertTrue(campo.isMarcado());
+    }
+
+    @Test
+    void testeAbrirNaoMinadoNaoMarcado(){
+        assertTrue(campo.abrir());
+    }
+    @Test
+    void testeAbrirNaoMinadoMarcado(){
+        campo.alternarMarcado();
+        assertFalse(campo.abrir());
+    }
+    @Test
+    void testeAbrirMinadoMarcado(){
+        campo.alternarMarcado();
+        campo.minar();
+        assertFalse(campo.abrir());
+    }
+    @Test
+    void testeAbrirMinadoNaoMarcado(){
+        campo.minar();
+
+        assertThrows(ExplosaoException.class, () -> {
+            campo.abrir();
+        });
+    }
+
+    @Test
+    void testeAbrirComVizinhos1(){
+
+        Campo campo11 = new Campo(1, 1);
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+
+        assertTrue(campo22.isAberto() && campo11.isAberto());
+    }
+
+    @Test
+    void testeAbrirComVizinhos2(){
+
+        Campo campo11 = new Campo(1, 1);
+        Campo campo12 = new Campo(1, 2);
+        campo12.minar();
+
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+
+        assertTrue(campo22.isAberto() && !campo11.isAberto());
     }
 }
