90e337c14e50a4c54e8e9e30ec29fb58f0e7cb6f
gusbdie <gustavobdietrich@gmail.com>
2025-01-15 21:39:35 -0300

    Criei essa classe pra ir testando a visualização do tabuleiro no terminal conforme vou desenvolvendo ele

---
+++ src/Aplicacao.java
@@ -0,0 +1,12 @@
+import modelo.Tabuleiro;
+
+public class Aplicacao {
+
+    public static void main(String[] args) {
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+    
+        System.out.println(tabuleiro); 
+        //criei essa classe para ir testando a implementação  
+        //da visão do jogo no console
+    }
+}
